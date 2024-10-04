2df856f17f52bdf1d0e7ef513fe3cbc4fb226ec4
Roberto Ruivo <148583419+DexPT@users.noreply.github.com>
2024-10-04 21:51:02 +0100

    Home created
    1st output

---
+++ views/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gamestore in the nighty</title>
+</head>
+<body>
+    <h1>Gamestore</h1>
+    <h6>v0.0.01</h6>
+</body>
+</html>
